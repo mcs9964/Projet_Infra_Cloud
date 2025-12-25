ba17b7ab44b38cbdb51ff39bcf2f7fc005fa0bb7
AtifHamzaN7 <162143582+AtifHamzaN7@users.noreply.github.com>
2025-12-26 00:53:50 +0100

    nouvelle config terraform

--- terraform/inventory.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/inventory.tpl", {
-    master_ip = google_compute_instance.master.network_interface[0].access_config[0].nat_ip
-    edge_ip   = google_compute_instance.edge.network_interface[0].access_config[0].nat_ip
-    worker_ips= [for w in google_compute_instance.worker : w.network_interface[0].access_config[0].nat_ip]
-    ssh_user  = var.ssh_username
-  })
-  filename = "${path.module}/../ansible/inventories/generated/hosts.ini"
-}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "master_public_ip"  { value = google_compute_instance.master.network_interface[0].access_config[0].nat_ip }
-output "edge_public_ip"    { value = google_compute_instance.edge.network_interface[0].access_config[0].nat_ip }
-output "worker_public_ips" { value = [for w in google_compute_instance.worker : w.network_interface[0].access_config[0].nat_ip] }
